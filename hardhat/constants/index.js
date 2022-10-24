d52afb2fd9ed2f0089f240d62ef78d2de54857f2
Raul Flores <raul.alexander.flores1@gmail.com>
2022-10-24 15:22:02 -0700

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,8 @@
+
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2D1Ad286aD0c037d556E77E8aB1a6D8e6717E203";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
